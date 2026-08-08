26d7078de5c5548cdc896d6119e0afbb7eae1ad1
manav-chotai081 <chotaimanav46@gmail.com>
2026-08-08 14:10:39 +0530

    Time: 0 ms (100%), Space: 19.2 MB (51.61%) - LeetHub

--- 2396-strictly-palindromic-number/2396-strictly-palindromic-number.py
+++ 2396-strictly-palindromic-number/2396-strictly-palindromic-number.py
@@ -1,14 +1,3 @@
 class Solution:
     def isStrictlyPalindromic(self, n: int) -> bool:
-        # for i in range(2,n-1):
-        #     copy = n
-        #     s = ''
-        #     while copy > 0:
-        #         s += str(copy%i)
-        #         copy = copy // i
-        #         if s == s[::-1]:
-        #             pass
-        #         else:
-        #             return False
-        #     return True  
         return False      
