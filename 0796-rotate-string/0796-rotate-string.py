2cbd915c8c46b584ae703089a1fad9b5cfd97167
manav-chotai081 <chotaimanav46@gmail.com>
2026-08-10 17:25:50 +0530

    Time: 0 ms (100%), Space: 19.1 MB (85.71%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.py
+++ 0796-rotate-string/0796-rotate-string.py
@@ -5,10 +5,10 @@ class Solution:
         # len1 = len(s)
         # for i in range(i1,len1):
         #     if s[i] != goal[i-i1]:
-        #         return False  
+        #         return False
         # return True
         for i in range(len(s)):
-            s = s[len(s)-1] + s
+            s = s[len(s) - 1] + s
             s = s[:-1]
             if s == goal:
                 return True
