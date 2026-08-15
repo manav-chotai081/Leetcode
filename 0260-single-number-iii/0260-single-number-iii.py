ca22ee27067126ff26c9edb3e4b103f4ab7b4d24
manav-chotai081 <chotaimanav46@gmail.com>
2026-08-15 19:02:08 +0530

    Time: 3922 ms (5.02%), Space: 20.8 MB (87.3%) - LeetHub

---
+++ 0260-single-number-iii/0260-single-number-iii.py
@@ -0,0 +1,9 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> List[int]:
+        ans = []
+        for i in nums:
+            if nums.count(i) == 1:
+                ans.append(i)
+        return ans 
+
+        
