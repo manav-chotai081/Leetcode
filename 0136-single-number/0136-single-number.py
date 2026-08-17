f136fd073760146717509bfbf2ab3fef2e9bbd0c
manav-chotai081 <chotaimanav46@gmail.com>
2026-08-17 14:32:35 +0530

    Time: 2653 ms (8.46%), Space: 21.9 MB (10.26%) - LeetHub

---
+++ 0136-single-number/0136-single-number.py
@@ -0,0 +1,8 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        num = list(set(nums))
+        
+        for i in num:
+            if nums.count(i) == 1:
+                return i
+        
