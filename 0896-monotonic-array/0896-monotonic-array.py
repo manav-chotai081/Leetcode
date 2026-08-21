081ffbb8a6902dcd695b5b749f3c46ed11cc7c62
manav-chotai081 <chotaimanav46@gmail.com>
2026-08-21 20:51:06 +0530

    Time: 44 ms (59.48%), Space: 31.9 MB (10.04%) - LeetHub

--- 0896-monotonic-array/0896-monotonic-array.py
+++ 0896-monotonic-array/0896-monotonic-array.py
@@ -2,7 +2,6 @@ class Solution:
     def isMonotonic(self, nums: List[int]) -> bool:
         temp = nums[::]
         temp.sort()
-        
         if nums == temp or nums == temp[::-1]:
             return True
         return False
