a08bb2df798e4eecc3bdda2a7f6eba3a079e1c1c
manav-chotai081 <chotaimanav46@gmail.com>
2026-08-25 15:43:14 +0530

    Time: 99 ms (13.26%), Space: 19.2 MB (61.61%) - LeetHub

--- 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.py
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.py
@@ -1,15 +1,15 @@
 class Solution:
     def trailingZeroes(self, n: int) -> int:
-        c2 = 0
+        # c2 = 0
         c5 = 0
         for i in range(1,n+1):
-            temp = i
-            while True:
-                if temp % 2 == 0:
-                    c2 += 1
-                    temp = temp // 2
-                else:
-                    break
+            # temp = i
+            # while True:
+            #     if temp % 2 == 0:
+            #         c2 += 1
+            #         temp = temp // 2
+            #     else:
+            #         break
             temp = i
             while True:
                 if temp % 5 == 0:
@@ -17,5 +17,5 @@ class Solution:
                     temp = temp // 5
                 else:
                     break 
-        return min(c2,c5)
+        return c5
         
