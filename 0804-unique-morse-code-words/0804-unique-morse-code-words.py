2f1cb675e40da09824a60c18201e5a8bc825c947
manav-chotai081 <chotaimanav46@gmail.com>
2026-08-31 11:35:47 +0530

    Time: 2 ms (30.13%), Space: 19.2 MB (64.96%) - LeetHub

---
+++ 0804-unique-morse-code-words/0804-unique-morse-code-words.py
@@ -0,0 +1,65 @@
+class Solution:
+    def uniqueMorseRepresentations(self, words: List[str]) -> int:
+        ans = []
+        for i in words:
+            temp = ''
+            for j in i:
+                match j:
+                    case 'a':
+                        temp += ".-"
+                    case 'b':
+                        temp += "-..."
+                    case 'c':
+                        temp += "-.-."
+                    case 'd':
+                        temp += "-.."
+                    case 'e':
+                        temp +="."
+                    case 'f':
+                        temp += "..-."
+                    case 'g':
+                        temp += "--."
+                    case 'h':
+                        temp += "...."
+                    case 'i':
+                        temp += ".."
+                    case 'j':
+                        temp += ".---"
+                    case 'k':
+                        temp += "-.-"
+                    case 'l':
+                        temp += ".-.."
+                    case 'm':
+                        temp += "--"
+                    case 'n':
+                        temp += "-."
+                    case 'o':
+                        temp += "---"
+                    case 'p':
+                        temp += ".--."
+                    case 'q':
+                        temp += "--.-"
+                    case 'r':
+                        temp += ".-."
+                    case 's':
+                        temp += "..."
+                    case 't':
+                        temp += "-"
+                    case 'u':
+                        temp += "..-"
+                    case 'v':
+                        temp += "...-"
+                    case 'w':
+                        temp += ".--"
+                    case 'x':
+                        temp += "-..-"
+                    case 'y':
+                        temp += "-.--"
+                    case 'z':
+                        temp += "--.."
+            ans.append(temp)
+        ans1 = list(set(ans))
+        return len(ans1)        
+
+
+        
