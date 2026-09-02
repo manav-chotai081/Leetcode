78ade69ffa969dc5b1d6d288c8f5529c35f7b0fc
manav-chotai081 <chotaimanav46@gmail.com>
2026-09-02 17:28:09 +0530

    Time: 2 ms (14.9%), Space: 19.4 MB (13.38%) - LeetHub

---
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.py
@@ -0,0 +1,29 @@
+class Solution:
+    def uniformArray(self, num1: list[int]) -> bool:
+        count = 0
+        flag = num1[0] % 2
+        for i in range(len(num1)):
+            if num1[i] % 2 == flag:
+                count += 1
+            else:
+                for j in range(len(num1)):
+                    if (num1[i] -num1[j]) % 2 == flag:
+                        count += 1
+                        break
+        if count == len(num1):
+            return True
+        count = 0
+        for i in range(len(num1)):
+            if num1[i] % 2 != flag:
+                count += 1
+            else:
+                for j in range(i+1,len(num1)):
+                    if (num1[i] -num1[j]) % 2 != flag:
+                        count += 1
+                        break
+        if count == len(num1):
+            return True
+        else:
+            return False
+
+        
